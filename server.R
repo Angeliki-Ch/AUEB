30a2949ce53c56c92c541aaf55d18cd543fb1999
StasinosKarampatsos <StasinosKarampatsos@users.noreply.github.com>
2015-06-29 19:27:03 +0300

    Update server.R
    quick and dirty way to fix plot y axis size in barplot for different values of max visitors

--- server.R
+++ server.R
@@ -3,6 +3,16 @@ library(shiny)
 shinyServer(function(input, output) {
   
   #Find maximum Number of visitors
+    m<-tplotdata[13,input$Museum]
+  
+  # calculating barplot ylim 
+    million<-as.logical(round(m/1000000))
+    hunthou<-as.logical(round(m/100000))
+    tenthou<-as.logical(round(m/10000))
+    thousan<-as.logical(round(m/1000))
+    hundred<-as.logical(round(m/100))
+    tendeka<-as.logical(round(m/10))
+    max<-m+million*1000000+hunthou*100000+tenthou*10000+thousan*1000+hundred*100+tendeka*10
   
   # Fill in the spot we created for a plot
   output$museumPlot <- renderPlot({

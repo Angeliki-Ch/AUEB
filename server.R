5f03bce4a9d9e6d6fbbdc7c469eb5bb8e4c12989
StasinosKarampatsos <StasinosKarampatsos@users.noreply.github.com>
2015-06-30 17:57:21 +0300

    Update server.R

--- server.R
+++ server.R
@@ -2,10 +2,11 @@ library(shiny)
 # Define a server for the Shiny app
 shinyServer(function(input, output) {
   
-  #Find maximum Number of visitors
+  # Fill in the spot we created for a plot
+  output$museumPlot <- renderPlot({
+    
+    # calculating barplot ylim
     m<-tplotdata[13,input$Museum]
-  
-  # calculating barplot ylim 
     million<-as.logical(round(m/1000000))
     hunthou<-as.logical(round(m/100000))
     tenthou<-as.logical(round(m/10000))
@@ -13,15 +14,13 @@ shinyServer(function(input, output) {
     hundred<-as.logical(round(m/100))
     tendeka<-as.logical(round(m/10))
     max<-m+million*1000000+hunthou*100000+tenthou*10000+thousan*1000+hundred*100+tendeka*10
-  
-  # Fill in the spot we created for a plot
-  output$museumPlot <- renderPlot({
     
     # Render a barplot
-    barplot(tplotdata[-13,input$Month], 
+    barplot(tplotdata[1:12,input$Museum], 
             main=input$Month,
             ylab="Number of Visitors",
             xlab="Month",
-            col="violetred")
+            col="violetred",
+            ylim=c(0, max))
   })
 })
